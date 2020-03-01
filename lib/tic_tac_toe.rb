fb712013e5fe34f646d892349577e54c14d8a89f
Jimmy Le <jimmyle414@gmail.com>
2020-03-01 23:22:37 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,66 @@
+WIN_COMBINATIONS = [
+[0, 1, 2], 
+[3, 4, 5], 
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]]
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts   "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts   "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+ def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  board[index] != " " && board[index] != " "
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+ input = gets.strip
+ index = input_to_index(input)
+ if valid_move?(board, index)
+   move(board, index)
+   display_board(board)
+ else
+   turn(board)
+ end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+      end
+      
+  end
+  counter
+end
+
+def current_player(board)
+   turn_count(board).even? ? "X" : "O"
+  end
+  
+
+  
+
