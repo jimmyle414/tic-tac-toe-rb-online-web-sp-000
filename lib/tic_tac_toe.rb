55364cf14c1528bac45ecdea32581762028279fe
Jimmy Le <jimmyle414@gmail.com>
2020-03-01 23:44:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,20 +1,19 @@
 WIN_COMBINATIONS = [
-[0, 1, 2], 
-[3, 4, 5], 
-[6, 7, 8],
-[0, 3, 6],
-[1, 4, 7],
-[2, 5, 8],
-[0, 4, 8],
-[2, 4, 6]]
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts   "-----------"
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts   "-----------"
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
@@ -27,11 +26,32 @@ def move(board, index, current_player)
 end
 
 def position_taken?(board, index)
-  board[index] != " " && board[index] != " "
+  !(board[index].nil? || board[index] == " ")
 end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn_count(board)
+  turn = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
+  end
+  return turn
+end
+
+def current_player(board)
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count(board)
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
 end
 
 def turn(board)
@@ -47,47 +67,56 @@ def turn(board)
   end
 end
 
-def turn_count(board)
-  counter = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      counter += 1
-      end
-      
-  end
-  counter
-end
-
-def current_player(board)
-   turn_count(board).even? ? "X" : "O"
-  end
-  
 def won?(board)
-WIN_COMBINATIONS.detect do |win_combination|
-  location1 = win_combination[0]
-  location2 = win_combination[1]
-  location3 = win_combination[2]
-  board[location1] == board[location2] && board[location2] == board[location3] && board[location1] != " "
-end
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
 end
 
 def full?(board)
-  board.none? {|position| position == " "}
+  board.all? {|index| index == "X" || index == "O"}
 end
 
 def draw?(board)
-  full?(board) && !won?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
 end
 
 def over?(board)
-  won?(board) || draw?(board)
+  if won?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
 end
 
-def winner(board)
-  win_combination = won?(board)
-  if win_combination
-    winning_location = win_combination[0]
-    board[winning_location]
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
 
@@ -102,8 +131,3 @@ def play(board)
     puts "Cats Game!"
   end
 end
- 
-
-
-  
-
